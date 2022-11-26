7b7176da23907be292f27cd2616cb50a342f8630
Alina Malashko <alinaa.mal03@gmail.com>
2022-11-27 00:36:05 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
 import React from "react";
-import Shop from "./components/Shop";
+import Router from "./components/Router.js";
+import { BrowserRouter } from "react-router-dom";
 
-const knives = {
-    kitchen: [{"id": 1, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ01-B", "price": 850, "mark": "Новинка"},
+const knives = 
+    [{"id": 1, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ01-B", "price": 850, "mark": "Новинка"},
             {"id": 2, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ02-B", "price": 860, "mark": ""},
             {"id": 3, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ03-B", "price": 870, "mark": ""},
-            {"id": 4, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ04-B", "price": 880, "mark": ""}],
-    folding: [{"id": 5, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ05-B", "price": 850, "mark": "Новинка"},
+            {"id": 4, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ04-B", "price": 880, "mark": ""},
+    {"id": 5, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ05-B", "price": 850, "mark": "Новинка"},
             {"id": 6, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ06-B", "price": 860, "mark": ""},
             {"id": 7, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ07-B", "price": 870, "mark": ""},
-            {"id": 8, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ08-B", "price": 880, "mark": ""}],
-    sharpeners: [{"id": 9, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ09-B", "price": 850, "mark": "Новинка"},
+            {"id": 8, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ08-B", "price": 880, "mark": ""},
+    {"id": 9, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ09-B", "price": 850, "mark": "Новинка"},
                 {"id": 10, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ10-B", "price": 860, "mark": ""},
                 {"id": 11, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ11-B", "price": 870, "mark": ""},
-                {"id": 12, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ12-B", "price": 880, "mark": ""}],
-    accessories: [{"id": 13, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ13-B", "price": 850, "mark": "Новинка"},
+                {"id": 12, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ12-B", "price": 880, "mark": ""},
+    {"id": 13, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ13-B", "price": 850, "mark": "Новинка"},
                 {"id": 14, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ14-B", "price": 860, "mark": ""},
                 {"id": 15, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ15-B", "price": 870, "mark": ""},
                 {"id": 16, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ16-B", "price": 880, "mark": ""},
                 {"id": 17, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ17-B", "price": 880, "mark": ""},
                 {"id": 18, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ18-B", "price": 880, "mark": ""},
                 {"id": 19, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ19-B", "price": 880, "mark": ""},
-                {"id": 20, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ20-B", "price": 880, "mark": ""}],
-            };
+                {"id": 20, "img": "https://i.postimg.cc/Vv3CYRcq/image-14.png", "name": "Складной нож SQ20-B", "price": 880, "mark": ""}];
 class App extends React.Component {
+    state = {
+        knives: knives,
+        kitchen: [...knives].splice(0, 4),
+        folding: [...knives].splice(4, 4),
+        sharpeners: [...knives].splice(8, 4),
+        accessories: [...knives].splice(12, 8)
+    };
     render() {
         return (
-            <Shop knives={knives}></Shop>
+            <BrowserRouter>
+                <Router knives={this.state.knives} kitchen={this.state.kitchen} folding={this.state.folding} sharpeners={this.state.sharpeners} accessories={this.state.accessories}></Router>
+            </BrowserRouter>
         )
     };
 };
---
+++ src/components/ShopSharpeners.js
@@ -0,0 +1,148 @@
+import React from "react";
+import "./Shop.css";
+import Item from "./Item.js";
+import { NavLink } from 'react-router-dom';
+
+class ShopSharpeners extends React.Component {
+    state = {
+        knives: this.props.knives,
+        sortedKnives: this.props.knives,
+        category: "sharpeners",
+        counter: localStorage.hasOwnProperty("clickedProductsDataArray") ? JSON.parse(localStorage.getItem("clickedProductsDataArray")).counter : 0,
+        sorted: "all",
+        clickedProductsDataArray: localStorage.hasOwnProperty("clickedProductsDataArray") ? JSON.parse(localStorage.getItem("clickedProductsDataArray")) : {counter: 0}
+    };
+    updateCurrent = (event) => {
+        if (event.target.closest(".Shop__Nav__btn")) {
+            if (event.target.closest(".Shop__Nav__btn").id === "kitchen") {
+                this.setState({category: "kitchen"});
+            };
+            if (event.target.closest(".Shop__Nav__btn").id === "folding") {
+                this.setState({category: "folding"});
+            };
+            if (event.target.closest(".Shop__Nav__btn").id === "sharpeners") {
+                this.setState({category: "sharpeners"});
+            };
+            if (event.target.closest(".Shop__Nav__btn").id === "accessories") {
+                this.setState({category: "accessories"});
+            };
+        }
+    };
+    sort = (event) => {
+        let option;
+        if (event.target.id === "new") {
+            option = "Новинка";
+            this.setState({sorted: "new"});
+        }
+        if (event.target.id === "all") {
+            option = "";
+            this.setState({sorted: "all"});
+        }
+        console.log(option)
+        let allKnives;
+        if (this.state.category === "kitchen") {
+          allKnives = [...this.state.kitchen];
+        }
+         if (this.state.category === "folding") {
+          allKnives = [...this.state.folding];
+        }
+         if (this.state.category === "sharpeners") {
+          allKnives = [...this.state.sharpeners];
+        }
+         if (this.state.category === "accessories") {
+          allKnives = [...this.state.accessories];
+        }
+        let sortedKnives = allKnives.filter(item => item.mark === option);
+        console.log(sortedKnives)
+    };
+    /*countPrice = (price) => {
+        let newPrice = this.state.counter + price;
+        //this.setState({counter: newPrice});
+    };*/
+    rememberClicked = (id, counter, price) => {
+        console.log(price)
+        let clickedArray = {...this.state.clickedProductsDataArray};
+        let newId = id;
+        clickedArray[newId] = counter;
+        clickedArray["counter"] += price;
+        console.log(clickedArray)
+        localStorage.setItem("clickedProductsDataArray", JSON.stringify(clickedArray));
+        this.setState({clickedProductsDataArray: clickedArray, counter: clickedArray["counter"]});
+    };
+    render() {
+        return (
+            <div>
+                <header className="Shop__banner">
+                    <div className="Shop__header">
+                        <a href="tel:89811201117"><img src="https://svgshare.com/i/oHH.svg" alt="Позвонить"></img></a>
+                        <img src="https://i.postimg.cc/D0bQc1Yj/logo.png" alt="Логотип"></img>
+                        <div className="Shop__header__cart">
+                            <img src="https://svgshare.com/i/oHS.svg" alt="Корзина"></img>
+                            {this.state.counter !== 0 ? 
+                                <p>{this.state.counter} р.</p>
+                            : ""
+                            }
+                        </div>
+                    </div>
+                    <div>
+                        <h1 className="Shop__heading">
+                            Исключительное качество без компромиссов
+                        </h1>
+                        <p className="Shop__subheading">
+                            Ножи «Tuotown» – это главный инструмент поваров и секрет кулинарного мастерства
+                        </p>
+                    </div>
+                </header>
+                <nav>
+                    <ul className="Shop__Nav">
+                        <NavLink to="/kitchen">
+                            <li className="Shop__Nav__btn" id="kitchen" onClick={this.updateCurrent}>
+                                <img src="https://svgshare.com/i/oG5.svg" alt="Кухонные ножи"></img>
+                                <p className={this.state.category === "kitchen" ? "active" : ""}>Кухонные ножи</p>
+                            </li>
+                        </NavLink>
+                        <NavLink to="/folding">
+                            <li className="Shop__Nav__btn" id="folding" onClick={this.updateCurrent}>
+                                <img src="https://svgshare.com/i/oH5.svg" alt="Складные ножи"></img>
+                                <p className={this.state.category === "folding" ? "active" : ""}>Складные ножи</p>
+                            </li>
+                        </NavLink>
+                        <NavLink to="/sharpeners">
+                            <li className="Shop__Nav__btn" id="sharpeners" onClick={this.updateCurrent}>
+                                <img src="https://svgshare.com/i/oH6.svg" alt="Точилки для ножей"></img>
+                                <p className={this.state.category === "sharpeners" ? "active" : ""}>Точилки для ножей</p>
+                            </li>
+                        </NavLink>
+                        <NavLink to="/accessories">
+                            <li className="Shop__Nav__btn" id="accessories" onClick={this.updateCurrent}>
+                                <img src="https://svgshare.com/i/oGQ.svg" alt="Аксессуары для ножей"></img>
+                                <p className={this.state.category === "accessories" ? "active" : ""}>Аксессуары для кухни</p>
+                            </li>
+                        </NavLink>
+                    </ul>
+                </nav>
+                <section className="Shop__categories">
+                    <ul>
+                        <li onClick={this.sort} id="all" className={this.state.sorted === "all" ? "sort" : ""}>Все</li>
+                        <li onClick={this.sort} id="new" className={this.state.sorted === "new" ? "sort" : ""}>Новинки</li>
+                    </ul>
+                </section>
+                <main className="Shop__content">
+                    {this.state.knives.map(el => {
+                        let newId = el.id.toString();
+                        if (this.state.clickedProductsDataArray.hasOwnProperty(newId)) {
+                            return <Item key={el.id} item={el} counter={this.state.clickedProductsDataArray[newId]} clicked={true} countPrice={this.countPrice} rememberClicked={this.rememberClicked}></Item>
+                        } else {
+                            return <Item key={el.id} item={el} counter={0} clicked={false} countPrice={this.countPrice} rememberClicked={this.rememberClicked}></Item>
+                        }
+                    })}
+                </main>
+                <footer className="Shop__footer">
+                    <p>© 2022 Все права защищены и принадлежат правообладателям.</p>
+                </footer>
+            </div>
+        );
+    };
+}
+
+export default ShopSharpeners;
